2cd9247e90296f9febcb400cb8f759a0c8947f14
JerryOffiicial <jerrysonjerry1234@email.com>
2026-01-02 17:10:17 +0530

    Cookies

--- EXPRESS/src/index.mjs
+++ EXPRESS/src/index.mjs
@@ -1,14 +1,16 @@
 import express from "express";
 import routes from "./routes/router.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser("Jerry"));
 app.use(routes);
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  //request handler
+app.get("/", (req, res) => {//request handler
+  res.cookie("user", "Admin", { maxAge: 60000 * 60, signed: true });
   res.send({ msg: "Root" });
 });
 
